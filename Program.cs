43f83fa8983115681d091c61823dedb7e2cc758f
dikoleshubh <dikole.shubh@gmail.com>
2021-05-11 19:09:38 +0530

    Linked List-Specific position node element insertion

--- Program.cs
+++ Program.cs
@@ -19,9 +19,10 @@ namespace Linkedlist
             //list.InsertFront(30);// Adding data in list //UC2
             //list.InsertFront(56);
 
-            list.Append(56);// Adding data in list //UC3
-            list.Append(30);
-            list.Append(70);
+            //list.Append(56);// Adding data in list //UC3
+            //list.Append(30);
+            //list.Append(70);
+            list.InsertAtParticularPosition(2, 30);//UC4
 
             list.Display();
             Console.ReadLine();
